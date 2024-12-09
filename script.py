d55838acfa737254564ab504ec68b0c9c689002d
Mlakshita12 <lakshitamadhavan12@gmail.com>
2024-12-09 14:03:10 -0500

    Automatically runs through directory

---
+++ script.py
@@ -0,0 +1,16 @@
+import os
+import yextract
+
+directory = 'testcases'
+
+ls = []
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        if f.endswith('.yaml'):
+            ls.append(f)
+
+yextract.run(ls)
+
+
+    
--- yextract.py
+++ yextract.py
@@ -3,13 +3,19 @@ import sys
 
 args = sys.argv
 
-data = yaml.safe_load(open(args[1]))
+def run(args):
+    for i in range(len(args)-1):
+        data = yaml.safe_load(open(args[1+i]))
+        print("File Name: \n\n",args[1+i])
 
-for job in data['jobs']:
-    count = 0
-    for step in data['jobs'][job]['steps']:
-        count += 1
-        print(f'{job}: step {count}: {step}')
+        for job in data['jobs']:
+            count = 0
+            for step in data['jobs'][job]['steps']:
+                count += 1
+                print(f'{job}: step {count}: {step}')
 
-        if 'run' in step:
-            print(f"MATCH: {step['run']}")
+                if 'run' in step:
+                    print(f"MATCH: {step['run']}")
+
+
+run(args)
